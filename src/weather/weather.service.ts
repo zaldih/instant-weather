5d68f44fcd8a849042b432d2ed4098c2dec97989
juanT <imzaldih@gmail.com>
2021-10-21 13:18:58 +0200

    refactor(weather.service): implement no-data exception

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,4 +1,5 @@
 import { HttpService } from 'src/shared/services/http.service';
+import { NoDataException } from './exceptions/no-data.exception';
 
 export class WeatherService {
   constructor(private http: HttpService = new HttpService()) {}
@@ -7,7 +8,14 @@ export class WeatherService {
     console.log('API');
     const API_KEY = 'nulk';
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&appid=${API_KEY}`;
-    return this.http.get(url);
+    return this.http
+      .get(url)
+      .then(async (response: { data: string }) => {
+        return response.data;
+      })
+      .catch((error: any) => {
+        throw new NoDataException();
+      });
   }
 
   getWetherFromCache(lat: number, lon: number): Promise<any> {
