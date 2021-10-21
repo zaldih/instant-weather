3a5dae1cd25f319743d787f5fec8df9cee126496
juanT <imzaldih@gmail.com>
2021-10-21 17:53:34 +0200

    refactor(weather.service): get api key from env var

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -2,13 +2,14 @@ import { plainToClass } from 'class-transformer';
 import { HttpService } from '../shared/services/http.service';
 import { NoDataException } from './exceptions/no-data.exception';
 import Wheather from './weather.model';
+import config from '../shared/config';
 
 export class WeatherService {
   constructor(private http: HttpService = new HttpService()) {}
 
   getWeather(lat: number, lon: number): Promise<any> {
     console.log('API');
-    const API_KEY = 'nulk';
+    const API_KEY = config.OPEN_WEATHER_KEY;
     const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&appid=${API_KEY}`;
     return this.http
       .get(url)
