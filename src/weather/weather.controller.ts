e0223a2fba028938d5cb4420a893ce7335e9b398
juanT <imzaldih@gmail.com>
2021-10-21 13:19:43 +0200

    refactor(weather.controller): return dto in #getWeater

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -1,6 +1,7 @@
 import { Router, NextFunction, Request, Response } from 'express';
 import { asyncHandler } from '../shared/handlers/async.handler';
 import { Controller } from '../shared/interfaces/controller.interface';
+import { WeatherDto } from './weather.dto';
 
 import { WeatherService } from './weather.service';
 
@@ -23,6 +24,8 @@ export class WeatherController implements Controller {
   private async getWeather(req: Request, res: Response) {
     const lat = +req.params.lat;
     const lon = +req.params.lon;
-    res.send('ok');
+    const weather = await this.weatherService.getWeather(lat, lon);
+    const weatherDto = new WeatherDto(weather);
+    res.json(weatherDto);
   }
 }
