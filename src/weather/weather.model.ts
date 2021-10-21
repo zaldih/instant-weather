92276c3b19b591c5a2178334b0940c4bea171d79
juanT <imzaldih@gmail.com>
2021-10-21 12:53:36 +0200

    feat(weather.model): calculate expirationDate

--- src/weather/weather.model.ts
+++ src/weather/weather.model.ts
@@ -7,7 +7,9 @@ export default class Wheather {
   daily: any[];
 
   constructor(lat: number, lon: number, hourly: any[], daily: any[]) {
-    this.timestamp = new Date().getTime();
+    const now = new Date().getTime();
+    this.timestamp = now;
+    this.expirationDate = this.calculateExpiration(now);
     this.lat = lat;
     this.lon = lon;
     this.hourly = hourly;
@@ -16,5 +18,13 @@ export default class Wheather {
 
   isValid() {}
 
-  private calculateExpiration() {}
+  private calculateExpiration(timestamp: number) {
+    const expirationMinutes = 3;
+    return this.addMinutes(timestamp, expirationMinutes);
+  }
+
+  // TODO move to other file
+  private addMinutes(timestamp: number, minutes: number) {
+    return timestamp + minutes * 60 * 1000;
+  }
 }
