614699713a88b49a7075aacc57a0339076eb16e7
juanT <imzaldih@gmail.com>
2021-10-21 12:44:39 +0200

    feat(weather.service): create #getWeather

--- src/weather/weather.service.ts
+++ src/weather/weather.service.ts
@@ -1,3 +1,17 @@
+import { HttpService } from 'src/shared/services/http.service';
+
 export class WeatherService {
-  constructor() {}
+  constructor(private http: HttpService = new HttpService()) {}
+
+  getWeather(lat: number, lon: number): Promise<any> {
+    console.log('API');
+    const API_KEY = 'nulk';
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=current,minutely,alerts&appid=${API_KEY}`;
+    return this.http.get(url);
+  }
+
+  getWetherFromCache(lat: number, lon: number): Promise<any> {
+    console.log('CACHE');
+    return null;
+  }
 }
