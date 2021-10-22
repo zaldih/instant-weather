165b51643905b25ff2eac1c4e6d96d5adc6d26f3
juanT <imzaldih@gmail.com>
2021-10-23 00:46:39 +0200

    perf(weather.repository): create index
    Index for lat and lon

--- src/weather/weather.repository.ts
+++ src/weather/weather.repository.ts
@@ -4,5 +4,10 @@ import Wheather from './weather.model';
 export class WeatherRepository extends BaseRepository<Wheather> {
   constructor() {
     super('weather');
+    this.createIndex();
+  }
+
+  private createIndex() {
+    return this.collection.createIndex({ lat: 1, lon: 1 });
   }
 }
