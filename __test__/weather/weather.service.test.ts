0e7c3a9e5743a80431a33b35c9c0f6474e7cab59
juanT <imzaldih@gmail.com>
2021-10-23 18:21:58 +0200

    test(weather): create test for #isValidHour

--- __test__/weather/weather.service.test.ts
+++ __test__/weather/weather.service.test.ts
@@ -1,5 +1,22 @@
+jest.mock('../../src/weather/weather.repository');
+jest.mock('../../src/shared/services/http.service');
+import { WeatherService } from '../../src/weather/weather.service';
+
 describe('WeatherService', () => {
-  it('hello', () => {
-    expect(true).toBeTruthy();
+  let weatherService: WeatherService;
+
+  beforeAll(() => {
+    weatherService = new WeatherService();
+  });
+
+  it('#isValidHour should return if a hour is valid', () => {
+    expect(weatherService.isValidHour(0)).toBeTruthy();
+    expect(weatherService.isValidHour(2)).toBeTruthy();
+    expect(weatherService.isValidHour(23)).toBeTruthy();
+    expect(weatherService.isValidHour(-1)).toBeFalsy();
+    expect(weatherService.isValidHour(5.2)).toBeFalsy();
+    expect(weatherService.isValidHour(24)).toBeFalsy();
+    expect(weatherService.isValidHour(null)).toBeFalsy();
+    expect(weatherService.isValidHour(undefined)).toBeFalsy();
   });
 });
